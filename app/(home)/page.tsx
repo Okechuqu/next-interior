b83cbb94a96a36997e47006123b62bd4a0150726
Okechuqu <chukwummnl@gmail.com>
2025-05-14 08:28:42 +0100

    add home page carousel

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,7 +1,6 @@
+import HomeCarousel from "@/components/shared/home/Home-carousel";
+import { data } from "@/lib/data";
+
 export default async function Page() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1>Home Page Content</h1>
-    </div>
-  );
+  return <HomeCarousel items={data.carousels} />;
 }
