cc0843ef4d91a1755d2c5655033e49c5a861503d
Okechuqu <chukwummnl@gmail.com>
2025-05-14 07:02:28 +0100

    add images

--- lib/data.ts
+++ lib/data.ts
@@ -28,5 +28,36 @@ export const data = {
       name: "Shop By Brand",
       href: "/search?tag=shop-by-brand",
     },
+    {
+      name: "Shop By Price",
+      href: "/search?tag=shop-by-price",
+    },
+    {
+      name: "Shop By Rating",
+      href: "/search?tag=shop-by-rating",
+    },
+  ],
+  carousels: [
+    {
+      title: "Most Popular Shoes For Sale",
+      buttonCaption: "Shop Now",
+      image: "/images/banner3.jpg",
+      url: "/search?category=Shoes",
+      isPublished: true,
+    },
+    {
+      title: "Best Sellers in T-Shirts",
+      buttonCaption: "Shop Now",
+      image: "/images/banner1.jpg",
+      url: "/search?category=T-Shirts",
+      isPublished: true,
+    },
+    {
+      title: "Best Deals on Wrist Watches",
+      buttonCaption: "See More",
+      image: "/images/banner2.jpg",
+      url: "/search?category=Wrist Watches",
+      isPublished: true,
+    },
   ],
 };
